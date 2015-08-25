aadc52142808d6792c2e037d38e1cd7865d13dda
mdaviscph <mdaviscph@gmail.com>
2015-08-24 21:16:17 -0700

    Changes to HuntDetailViewController, HuntDetailCell and Storyboard to remove segmented control and to use Hunt and CheckPoint class and struct to back the tableView. Added header view for hunt name and labels to HuntDetailCell.xib. Auto dimensioning of tableView cells not working yet.

--- Scavenger/HuntDetailViewController.swift
+++ Scavenger/HuntDetailViewController.swift
@@ -5,49 +5,67 @@
 //  Created by Sarah Hermanns on 8/24/15.
 //  Copyright (c) 2015 Chris Budro. All rights reserved.
 //
+//  Hunt User List View
 
 import UIKit
 
 class HuntDetailViewController: UIViewController {
   
-  @IBOutlet weak var tableView: UITableView!
+  let navigationTitle = "Hunt (User List View)"
   
-  @IBAction func segmentedControl(sender: UISegmentedControl) {
-    if sender.selectedSegmentIndex == 0 {
-      tableView.reloadData()
-      
-    } else {
-//      this is where we want to load the general bounds map view
+  // MARK: Public Properties
+  var hunt: Hunt? {
+    didSet {
+      updateUI()
     }
   }
   
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      
-      tableView.estimatedRowHeight = 70
-      tableView.rowHeight = UITableViewAutomaticDimension
-      
+  // MARK: IBOutlets, IBActions
+  @IBOutlet weak var huntNameLabel: UILabel!
+  @IBOutlet weak var tableView: UITableView! {
+    didSet {
       tableView.registerNib(UINib(nibName: "HuntDetailCell", bundle: NSBundle.mainBundle()), forCellReuseIdentifier: "HuntDetailCell")
-
-
-        navigationItem.title = "Hunt Detail"
+      tableView.dataSource = self
+      //tableView.estimatedRowHeight = tableView.rowHeight
+      //tableView.rowHeight = UITableViewAutomaticDimension
+      tableView.rowHeight = 160
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  }
+  @IBAction func segmentedControl(sender: UISegmentedControl) {}
+  
+  // MARK: Lifecycle Methods
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    navigationItem.title = navigationTitle
+    
+    // stuff some data in to test
+    hunt = Hunt(name: "Pike Place Market, Seattle", creatorName: "mdavis", huntDescription: "A way cool hunt through Pike Place Market")
+    let cp1 = CheckPoint(locationName: "Checkpoint 1", detail: "Enter the market from the NE corner and look for a vender selling local brands of honey. Find the fish painting nearby.", location: 180.0)
+    let cp2 = CheckPoint(locationName: "Checkpoint 2", detail: "Choose the staircase the takes you one level below the public restrooms and find exit that faces Puget Sound. Locate a unique but very ugly statue.", location: -180.0)
+    let cp3 = CheckPoint(locationName: "Checkpoint 3", detail: "Enter the market from the NE corner and look for a vender selling local brands of honey. Find the fish painting nearby.", location: 180.0)
+    let cp4 = CheckPoint(locationName: "Checkpoint 4", detail: "Choose the staircase the takes you one level below the public restrooms and find exit that faces Puget Sound. Locate a unique but very ugly statue.", location: -180.0)
+    hunt!.checkpoints += [cp1, cp2, cp3, cp4]
+  }
+  
+  // MARK: Private Helper Methods
+  private func updateUI() {
+    huntNameLabel?.text = hunt?.name
+    tableView.reloadData()
+  }
 }
 
+// MARK: UITableViewDataSource
 extension HuntDetailViewController: UITableViewDataSource {
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    if let count = hunt?.checkpoints.count {
+      return count
+    }
+    return 0
   }
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("HuntDetailCell", forIndexPath: indexPath) as! HuntDetailCell
-  
+    cell.checkPoint = hunt?.checkpoints[indexPath.row]
     return cell
   }
 }
